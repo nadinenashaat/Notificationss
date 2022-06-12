f6313c820b13eafba3af410c1ad9bacfc84b0da8
nashhhh <nadeen.abdelaal@student.giu-uni.de>
2022-06-12 23:58:51 +0200

    m

--- api/index.js
+++ api/index.js
@@ -1,58 +1,27 @@
 const sgmail = require('@sendgrid/mail');
+const { response } = require('express');
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const req = require('express/lib/request');
 const app = express();
-const cors=require ('cors');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors({ 
-  origin:"http://localhost:3000"
-}))
-
-sgmail.setApiKey(process.env.SENDGRID_API_KEY);
-
-/*app.post('/api/payment', async function payments(req, res) {
-  const message = {
-    to: req.body.email,
-    from: 'martrabbits@gmail.com',
-    subject: 'payment confirmation',
-    text: "thank you for using rabbitsmart!we've successfully processed your payment "
-  };
-  await sgmail.send(message);
-  return res.json(message);
-});
 
 
-app.post('/api/ship', async function shipping(req, res) {
-  const message = {
-    to: req.body.email,
-    from: 'martrabbits@gmail.com',
-    subject: 'shipping update',
-    text: " your order is being shipped"
-  };
-  await sgmail.send(message);
-  return res.json(message);
-});*/
 
+sgmail.setApiKey('SG.FN73eWCtSEGlLovFgm-6vg.CyXLFZwIem99hv1wcKytmLt8gRX7TZKcyFcRmlgfh9g')
 app.post('/api/order', async function order(req, res) {
-  const message = {
-    to: req.body.email,
-    from: 'martrabbits@gmail.com',
-    subject: 'order update',
-    text: " your order is created",
-  };
-  await sgmail.send(message,(error, response) => {
-    if(error) {
-        console.log(error);
-    }
-    else{
-        console.log(response);
-    }
- });
-  return res.json(message);
-});
-app.get('/',(req,res)=> {res.send('succces')})
+const msg = {
+  to: "nadinenashttt@gmail.com", 
+  from: "martrabbits@gmail.com", 
+  subject: "order update",
+  text: "order created",
+ 
+  
+}});
+
+sgmail.send(msg).then(response =>console.log('emailsent'))
+.catch(error=> console.log(error.msg))
+console.log(msg.status)
 
 app.listen(3000);
+
